bb4cd3864a142d3cc5b1d1679830767f12b7c3e1
magi821214 <magi821214@gmail.com>
2019-11-16 17:03:10 +0800

    20191116

---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    public Transform TransformA;
+    public Transform TransformB;
+    public Camera cam;
+    public AudioSource AudioS;
+
+    private void Start()
+    {
+        //print(Random.value);
+        print("PI : " + Mathf.PI);
+        Camera.main.depth = Camera.main.depth + 5;
+        print(Random.Range(1, 500));
+
+        print("物件A的座標 : " + TransformA.position);
+        //TransformA.position = new Vector3(-1,1,0);
+        //TransformA.Rotate(0,0,1);
+        if (AudioS.isPlaying)
+        {
+            AudioS.playOnAwake = false;
+            print("遊戲音效 : " + TransformA.position);
+        }
+        else
+        {
+            AudioS.playOnAwake = true;
+        }
+
+        print("攝影機深度 : " + cam.depth);
+    }
+    private void Update()
+    {
+        TransformA.position = new Vector3(-1, 1, 0);
+        TransformA.Rotate(0, 1, 1);
+    }
+}
---
+++ UnityProject/Assets/Scripts/LearnIF.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public bool test;
+    public string prop;
+    public int _HP=100;
+    //一秒執行60次
+    private void Update()
+    {
+        #region 練習if 
+        //if (test)
+        //{
+        //    print("打開開關~");
+        //}
+        //else
+        //{
+        //    print("~關掉囉");
+        //}
+        if (_HP >= 70)
+        {
+            print("安全");
+        }
+        else if (_HP >= 50 && _HP < 70)
+        {
+            print("警告");
+        }
+        else if (_HP >= 20 && _HP < 50)
+        {
+            print("危險");
+        }
+        else if (_HP > 0 && _HP < 20)
+        {
+            print("快死了");
+        }
+        else if (_HP <= 0)
+        {
+            print("你死了");
+        }
+        else
+        {
+            print("??????");
+        }
+        #endregion
+
+    }
+}
